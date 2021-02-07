da964f4e71fc32dbd930d7a6edac90f30ba7c9a2
Niaz Mahmud Akash <77677494+Viperz75@users.noreply.github.com>
2021-02-07 12:01:58 +0600

    Delete Cinderella_Help.py

--- Cinderella_Help.py
+++
@@ -1,6 +0,0 @@
-help_list = 'You can ask me these things, such as', 'Time , I can calculate, Joke, Open browser, Open Youtube, ' \
-                                                    'Open Facebook, Open or Close applications, Search in Wikipedia, ' \
-                                                    'Play videos in Youtube, Search in Google', 'Search in Bing', \
-            'Search in DuckDuckGo',
-'Learn how to make or do something. Activate it by saying "activate how to" and exit by saying "exit"', \
-            'Switch Window', 'Temperature of you current location'
